e741d3f9229fcdacf97e31fedbb0242f906c1fc4
menglili <melelee@qq.com>
2023-05-26 21:59:36 +0800

    调整文档格式，适配python3.11

--- Mysql2docx/Column.py
+++ Mysql2docx/Column.py
@@ -7,13 +7,15 @@ class Column:
     allowNull=''
     defaultValue=''
     comment=''
+    primaryKey=''
 
-    def __init__(self, name,type,allowNull,defaultValue,comment):
+    def __init__(self, name,type,allowNull,defaultValue,comment,primaryKey):
         self.name = name
         self.type=type
         self.allowNull=allowNull
         self.defaultValue=defaultValue
         self.comment = comment
+        self.primaryKey = primaryKey
 
     def __str__(self):
         return self.name + "\t" + self.type + "\t" + self.allowNull + "\t" + self.defaultValue+"\t"+self.comment
